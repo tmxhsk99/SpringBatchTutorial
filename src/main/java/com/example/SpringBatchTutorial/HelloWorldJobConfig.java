ccc0fb1e71eac48df4434c5ab8f8584ec1cf5e18
juhyeon <tmxhsk99@naver.com>
2023-09-06 08:24:16 +0900

    feat : 기본적인 helloWorldJob 추가
    - 실행 시 ProgramArgument 로 helloWorldJob 지정하기
    - 예시: --spring.batch.job.names=helloWorldJob

---
+++ src/main/java/com/example/SpringBatchTutorial/HelloWorldJobConfig.java
@@ -0,0 +1,56 @@
+package com.example.SpringBatchTutorial;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class HelloWorldJobConfig {
+
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+
+
+    @Bean
+    public Job helloWorldJob() {
+        return jobBuilderFactory.get("helloWorldJob") // 이름을 정해준다.
+                .incrementer(new RunIdIncrementer())
+                .start(helloWorldStep())
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step helloWorldStep() {
+        return stepBuilderFactory.get("helloWorldStep")
+                .tasklet(helloWorldTasklet())
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Tasklet helloWorldTasklet() {
+        return new Tasklet() {
+            @Override
+            public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                System.out.println("Hello, World! Spring Batch!");
+                return RepeatStatus.FINISHED;
+            }
+        };
+    }
+}
