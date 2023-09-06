0643729f988e4bd0cdbf1140ab5711a9eee826d4
juhyeon <tmxhsk99@naver.com>
2023-09-07 07:57:43 +0900

    chore : 패키지 위치 변경

--- src/main/java/com/example/SpringBatchTutorial/job/helloworld/HelloWorldJobConfig.java
+++ src/main/java/com/example/SpringBatchTutorial/job/helloworld/HelloWorldJobConfig.java
@@ -1,4 +1,4 @@
-package com.example.SpringBatchTutorial.job;
+package com.example.SpringBatchTutorial.job.helloworld;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
