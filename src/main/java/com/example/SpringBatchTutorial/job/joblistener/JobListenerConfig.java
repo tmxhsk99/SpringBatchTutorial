9e8221c606de572c0f21b5829fd05fd8078f7580
juhyeon <tmxhsk99@naver.com>
2023-09-08 07:57:34 +0900

    feat : 기본적인 JobListenerConfig 작성

---
+++ src/main/java/com/example/SpringBatchTutorial/job/joblistener/JobListenerConfig.java
@@ -0,0 +1,52 @@
+package com.example.SpringBatchTutorial.job.joblistener;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * desc :
+ * run : --spring.batch.job.names=jobListenerJob
+ */
+@Configuration
+@RequiredArgsConstructor
+public class JobListenerConfig {
+
+    private final JobBuilderFactory jobBuilderFactory;
+
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job jobListenerJob(Step jobListenerStep) {
+        return jobBuilderFactory.get("jobListenerJob") // 이름을 정해준다.
+                .incrementer(new RunIdIncrementer())
+                .start(jobListenerStep)
+                .build();
+    }
+
+    @JobScope
+    @Bean
+    public Step jobListenerStep(Tasklet jobListenerStepTasklet) {
+        return stepBuilderFactory.get("jobListenerStep")
+                .tasklet(jobListenerStepTasklet)
+                .build();
+    }
+
+    @StepScope
+    @Bean
+    public Tasklet jobListenerStepTasklet() {
+        return (contribution, chunkContext) -> { // new Tasklet() { ... } 대신 람다식을 사용할 수 있다.
+            System.out.println("Job Listener Tasklet");
+            return RepeatStatus.FINISHED;
+        };
+    }
+}
