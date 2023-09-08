14bc55b4cab3083b418b087bad2b259099e2d8a8
juhyeon <tmxhsk99@naver.com>
2023-09-08 09:17:48 +0900

    feat : Processor, ItemWriter 추가

--- src/main/java/com/example/SpringBatchTutorial/job/dbdatareadwrite/TrMigrationConfig.java
+++ src/main/java/com/example/SpringBatchTutorial/job/dbdatareadwrite/TrMigrationConfig.java
@@ -1,10 +1,12 @@
 package com.example.SpringBatchTutorial.job.dbdatareadwrite;
 
 
+import com.example.SpringBatchTutorial.core.domain.accounts.Accounts;
 import com.example.SpringBatchTutorial.core.domain.accounts.AccountsRepository;
 import com.example.SpringBatchTutorial.core.domain.orders.Orders;
 import com.example.SpringBatchTutorial.core.domain.orders.OrdersRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
@@ -12,16 +14,20 @@ import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.data.RepositoryItemReader;
+import org.springframework.batch.item.data.RepositoryItemWriter;
 import org.springframework.batch.item.data.builder.RepositoryItemReaderBuilder;
+import org.springframework.batch.item.data.builder.RepositoryItemWriterBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.Sort;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -30,6 +36,7 @@ import java.util.List;
  */
 @Configuration
 @RequiredArgsConstructor
+@Slf4j
 public class TrMigrationConfig {
 
     private final JobBuilderFactory jobBuilderFactory;
@@ -47,17 +54,61 @@ public class TrMigrationConfig {
 
     @JobScope
     @Bean
-    public Step trMigrationStep(ItemReader trOrdersReader) {
+    public Step trMigrationStep(ItemReader trOrdersReader,ItemProcessor trOrdersProcessor, ItemWriter trOrdersRepositoryItemWriter) {
         return stepBuilderFactory.get("trMigrationStep")
-                .<Orders, Orders>chunk(5)// ItemReader의 경우 어떤데이터로 읽어서 어떤데이터로 쓸건지 명시해야한다 / 5개 단위로 데이터를 처리하겠다 처리할데이터의 트랜잭션 사이즈 5개의 데이터를 커밋
+                .<Orders, Accounts>chunk(5)// ItemReader의 경우 어떤데이터로 읽어서 어떤데이터로 쓸건지 명시해야한다 / 5개 단위로 데이터를 처리하겠다 처리할데이터의 트랜잭션 사이즈 5개의 데이터를 커밋
                 .reader(trOrdersReader)
-                .writer(new ItemWriter() {
-                    @Override
-                    public void write(List items) throws Exception {// 단순히 출력만 수행한다.
-                        items.forEach(System.out::println); // 출력
-                    }
-                })
+                .processor(trOrdersProcessor)
+                .writer(trOrdersRepositoryItemWriter)
                 .build();
+    }
+
+    /**
+     * RepositoryItemWriter
+     * 읽어들인 값을 Repostiory를 사용해 DB에 저장하는 역할
+     * @return
+     */
+    @Bean
+    @StepScope
+    public RepositoryItemWriter<Accounts> trOrdersRepositoryItemWriter() {
+        log.info("trOrdersRepositoryItemWriter 호출 !!!");
+        return new RepositoryItemWriterBuilder<Accounts>()
+                .repository(accountsRepository)
+                .methodName("save")
+                .build();
+    }
+
+
+    /**
+     * 그냥 ItemWriter로 도 구현이 가능하다 이런 경우 직접 Repository를 호출하건 DB를 호출하건 해야한다.
+     * @return
+     */
+    public ItemWriter<Accounts> trOrdersItemWriter() {
+        log.info("trOrdersItemWriter 호출 !!!");
+        return new ItemWriter<Accounts>() {
+            @Override
+            public void write(List<? extends Accounts> items) throws Exception {
+                items.forEach(item -> accountsRepository.save(item));
+            }
+        };
+    }
+
+    /**
+     * ItemReader로 읽어들인값을 결과 값으로 파싱하는 역할을 하는 함수이다.
+     * @return
+     */
+    @StepScope
+    @Bean
+    public ItemProcessor<Orders,Accounts> trOrdersProcessor() {
+        return orders -> {
+            return Accounts.builder()
+                    .id(orders.getId())
+                    .orderItem(orders.getOrderItem())
+                    .orderDate(orders.getOrderDate())
+                    .accountDate(new Date())
+                    .price(orders.getPrice())
+                    .build();
+        };
     }
 
     @Bean
