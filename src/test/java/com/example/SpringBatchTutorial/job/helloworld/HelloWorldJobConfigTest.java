f48ed7100e510b2afba5a7d83fdd475a45743320
juhyeon <tmxhsk99@naver.com>
2023-09-12 08:10:38 +0900

    chore : 사용하지 않는 import 삭제

--- src/test/java/com/example/SpringBatchTutorial/job/helloworld/HelloWorldJobConfigTest.java
+++ src/test/java/com/example/SpringBatchTutorial/job/helloworld/HelloWorldJobConfigTest.java
@@ -14,7 +14,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
 @ActiveProfiles(profiles = "test")
